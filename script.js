29322746fc3caea8d48c9e1bf4a6aa2a0ef53b0c
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-04-28 12:06:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+function getTime() {
+    const time = new Date();
+    const dateString = `${time.getDate()}/${time.getMonth()}/${time.getFullYear()}`;
+
+    let hr = time.getHours();
+    let timeString = "";
+
+    if (hr >= 12) {
+        // pm
+        if (hr === 12) {
+            // no modification
+            timeString = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} PM`;
+        } else {
+            hr -= 12;
+            timeString = `${hr}:${time.getMinutes()}:${time.getSeconds()} PM`;
+        }
+    } else {
+        // am
+        timeString = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} AM`;
+    }
+    return dateString + ", " + timeString;
+}
+const para = document.getElementById('timer');
+
+function timeGenerate() {
+    para.innerText = getTime();
+}
+setInterval(timeGenerate, 1000);
